84d0a9fbeede6264a3db0e78cd4ea5f61d33952f
vdella <vitor.origamer@gmail.com>
2025-04-09 14:50:22 -0300

    added Pong stub

---
+++ train.py
@@ -0,0 +1,23 @@
+import gymnasium
+import ale_py
+
+gymnasium.register_envs(ale_py)
+
+
+def main():
+    env = gymnasium.make("ALE/Pong-v5", render_mode="human")
+    env.reset()
+
+    for _ in range(100):
+        action = env.action_space.sample()
+
+        obs, reward, terminated, truncated, info = env.step(action)
+
+        if terminated or truncated:
+            obs, info = env.reset()
+
+    env.close()
+
+
+if __name__ == "__main__":
+    main()
